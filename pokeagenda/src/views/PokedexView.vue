a0cb0f2c65fbca5d50cb56af42b8692e3667d6fe
MataLentajas <arturorient1305@gmail.com>
2025-02-02 19:40:26 +0100

    Agrega vistas de Pokedex y Perfil con navegación y diseño mejorado; incluye barra de búsqueda para Pokémon

--- pokeagenda/src/views/PokedexView.vue
+++ pokeagenda/src/views/PokedexView.vue
@@ -1,9 +1,29 @@
 <template>
-    <div></div>
+  <div class="min-h-screen bg-yellow-100">
+  <nav class="flex items-center justify-between p-4 bg-orange-800">
+    <div>
+      <img class="w-24" src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" alt="Logo de Pokémon">
+    </div>
+    <ul class="flex space-x-4">
+      <li><router-link to="/profile"><button class="cursor-pointer px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">Perfil</button></router-link></li>
+    </ul>
+  </nav>
+
+  <div class="flex justify-center mt-4">
+    <input type="text" placeholder="Buscar Pokémon favorito" class="px-4 py-2 border rounded-l-md focus:outline-none" v-model="searchQuery"/>
+    <button class="px-4 py-2 font-bold text-white bg-blue-500 rounded-r-md hover:bg-blue-700" @click="searchPokemon">Buscar</button>
+  </div>
+  </div>
   </template>
   
   <script>
   export default {
+    name: "PokedexView",
+    data(){
+      return{
+        
+      }
+    }
   
   }
   </script>
--- pokeagenda/src/views/ProfileView.vue
+++ pokeagenda/src/views/ProfileView.vue
@@ -1,6 +1,33 @@
 <template>
-    <div></div>
-  </template>
+  <div class="min-h-screen bg-yellow-100">
+  <nav class="flex items-center justify-between p-4 bg-orange-800">
+    <div>
+      <img class="w-24" src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" alt="Logo de Pokémon">
+    </div>
+    <ul class="flex space-x-4">
+      <li><router-link to="/pokedex"><button class="cursor-pointer px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">Pokedex</button></router-link></li>
+    </ul>
+  </nav>
+  <div>
+
+  </div>
+  <div class="container mx-auto p-4">
+    <div class="bg-white shadow-md rounded-lg p-6">
+      <div class="flex items-center space-x-4">
+        <div>
+          <h2 class="text-2xl font-bold">Nombre del Usuario</h2>
+          <p class="text-gray-600">Correo electrónico: </p>
+        </div>
+      </div>
+      <div class="mt-6">
+        <h3 class="text-xl font-semibold">Pokémon Favoritos</h3>
+      </div>
+    </div>
+  </div>
+
+  </div>
+</template>
+  
   
   <script>
   export default {
@@ -10,4 +37,4 @@
   
   <style>
   
-  </style>
+  </style>stylestyle
